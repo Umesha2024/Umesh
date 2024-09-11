409866f17a3ac2b26a4062082b80b6cf3c2e31c0
Umesha2024 <ua3359090@gmail.com>
2024-09-11 15:47:19 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
-
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char str1[50],str2[50];
+  printf("enter string1 : ");
+  scanf("%s",str1);
+  printf("Enter string2 : ");
+  scanf("%s",str2);
+  strcat(str1,str2);
+  printf("%s",str1);
 }
